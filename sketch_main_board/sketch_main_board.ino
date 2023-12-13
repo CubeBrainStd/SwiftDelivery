5787433773875b5562d47cac969daafe7f75e21c
Braismp007 <90138396+Braismp007@users.noreply.github.com>
2023-12-13 20:34:15 +0100

    He añadido el archivo de configuración principal de la placa
    He añadido el archivo principal del controlador central del robot, este código nuevo será modificado a medida que se vaya avanzando en el desarrollo del robot.

---
+++ sketch_main_board/sketch_main_board.ino
@@ -0,0 +1,21 @@
+//+------------------------------------------+
+//TITULO: PROGRAMACION PLACA PRINCIPAL IBIZA
+//ENGINER: Braismp007, TRIKIANAIS
+//FECHA-IN: 13/12/2023
+//VERSION: Version_Ibiza_1.1312.23.A
+//STATUS: IN DEVELOP - NO TESTING
+
+//(CC) CubeBrain Labs - Open Source Code
+//+------------------------------------------+
+
+#include <Arduino_GroveI2C_Ultrasonic.h>
+
+void setup() {
+ 
+
+}
+
+void loop() {
+  
+
+}
